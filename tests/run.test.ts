3d230813ee4d0bf957c8b3439b626945eb1143e5
raspo <raspaccio@gmail.com>
2022-03-03 13:59:49 -0800

    make it pass

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -260,7 +260,7 @@ describe('run', () => {
         });
         await run();
         expect(getCoverageMock).toBeCalledTimes(2);
-        expect(switchBranchMock).toBeCalledWith('888');
+        expect(switchBranchMock).toBeCalledWith('123');
     });
 
     it('should skip if headCoverage is not generated', async () => {
